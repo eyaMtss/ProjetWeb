8435d8b895113dfc1a425512f2c8b2f6239a31ae
eyaMtss <eya.mattoussii@gmail..com>
2021-01-30 10:16:12 +0100

    commit

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from 'app/app.component';
+import { ParticipantComponent } from 'app/Participants/participant/participant.component';
 import { AcceuilComponent } from 'app/acceuil/acceuil.component';
-import { DashboardComponent } from 'app/Dashboard/dashboard/dashboard.component';
 import { FormateursComponent } from 'app/Formateurs/formateurs/formateurs.component';
 import { SessionsComponent } from 'app/Sessions/sessions/sessions.component';
-import { ParticipantComponent } from 'app/Participants/participant/participant.component';
+import { DashboardComponent } from 'app/Dashboard/dashboard/dashboard.component';
 import { AjoutParticipantComponent } from 'app/Participants/participant/ajout-participant/ajout-participant.component';
 import { AjoutFormateurComponent } from 'app/Formateurs/formateurs/ajout-Formateur/ajout-formateur/ajout-formateur.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -10,21 +10,19 @@ import { AcceuilComponent } from './acceuil/acceuil.component';
 import { FormateursComponent } from './Formateurs/formateurs/formateurs.component';
 import { SessionsComponent } from './Sessions/sessions/sessions.component';
 import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
-import { ParticipantComponent } from './Participants/participant/participant.component';
 import { AjoutParticipantComponent } from './Participants/participant/ajout-participant/ajout-participant.component';
-import { ParticipantsComponent } from './Participants/participant/participant/participants/participants.component';
+import { ParticipantComponent } from './Participants/participant/participant.component';
 import { AjoutFormateurComponent } from './Formateurs/formateurs/ajout-Formateur/ajout-formateur/ajout-formateur.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParticipantComponent,
     AcceuilComponent,
     FormateursComponent,
     SessionsComponent,
     DashboardComponent,
     AjoutParticipantComponent,
-    ParticipantsComponent,
+    ParticipantComponent,
     AjoutFormateurComponent,
   ],
   imports: [
@@ -32,6 +30,7 @@ import { AjoutFormateurComponent } from './Formateurs/formateurs/ajout-Formateur
     FormsModule,
     HttpModule,
     AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
